0c08c57b20913bc0f204efad1a8864795af2e9b3
Wilson <cewilson19@gmail.com>
2013-06-17 20:21:36 -0400

    chpt6

---
+++ chpt6.rb
@@ -0,0 +1,9 @@
+puts 'What do you want?!'
+answer = gets.chomp
+puts 'ARE YOU KIDDING? YOU WANT "' + answer.upcase + '" THATS CRAZY. YOUR FIRED!'
+
+
+puts ('Table of Contents'.center(50))
+puts ('Chapter 1:    Getting Started'.ljust(40)) + ('page  1'.rjust(30))
+puts ('Chapter 2:    Numbers'.ljust(40)) + ('page  9'.rjust(30))
+puts ('Chapter 3:    letters'.ljust(40)) + ('page  13r'.rjust(30))
